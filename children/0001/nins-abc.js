7d9d3e854c4ac0578b1a4147b8644bb140a9bf0c
duguaynins <duguaynins@gmail.com>
2026-01-22 19:56:20 +0800

    Update nins-abc.js

--- children/0001/nins-abc.js
+++ children/0001/nins-abc.js
@@ -23,7 +23,7 @@ async function loadAllAssets() {
   ///await loadCSS('https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css');
   ///await loadJS('https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js');
   ///await loadJS('/js/map-widget.js');
-  await loadCSS('https://copyright.nins.cc/children/0001/nins-pirozu.css');
+  await loadCSS('https://copyright.nins.cc/children/0001/nins-zzz.css');  ///pirozu
   await loadJS('https://copyright.nins.cc/children/0001/nins-update.js');
   await loadJS('https://copyright.nins.cc/children/0001/nins-world.js');
   ///await loadJS('https://copyright.nins.cc/children/0001/nins-ads.js');
