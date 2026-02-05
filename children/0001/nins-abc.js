bc379799bf5f24e8288ebf33b760e96581b9fb1f
duguaynins <duguaynins@gmail.com>
2026-02-05 15:38:53 +0800

    Update nins-abc.js

--- children/0001/nins-abc.js
+++ children/0001/nins-abc.js
@@ -25,11 +25,12 @@ async function loadAllAssets() {
   ///await loadJS('/js/map-widget.js');
   ///await loadJS('https://copyright.nins.cc/children/0001/nins-update.js');
   
+  await loadJS('https://copyright.nins.cc/children/0001/nins-world.js');
+  ///await loadJS('https://copyright.nins.cc/children/0001/nins-ads.js');
+  
   await loadCSS('https://copyright.nins.cc/children/0001/nins-zzz.css');  ///pirozu
   await loadJS('https://copyright.nins.cc/children/0001/nins-reload.js');  
   await loadJS('https://copyright.nins.cc/children/0001/nins-dialog.js');  
-  ///await loadJS('https://copyright.nins.cc/children/0001/nins-world.js');
-  ///await loadJS('https://copyright.nins.cc/children/0001/nins-ads.js');
   
   await loadJS('https://copyright.nins.cc/children/0001/nins-translate.js');
   await loadJS('https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
