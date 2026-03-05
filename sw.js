28df6cb513daf19f4ade4500a3845c196ccc0b6a
duguaynins <duguaynins@gmail.com>
2026-03-05 20:12:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 self.addEventListener('install', event => {
   console.log('Service Worker 安裝完成');
+  self.skipWaiting(); // 立即進入 activate
 });
 
 self.addEventListener('activate', event => {
   console.log('Service Worker 啟動');
+  event.waitUntil(clients.claim()); // 接管所有頁面
 });
 
 self.addEventListener('fetch', event => {
