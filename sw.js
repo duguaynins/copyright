1d33a3ca4b3a737e3f6576b28232068d9d9a9588
duguaynins <duguaynins@gmail.com>
2026-03-30 20:24:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,18 +2,22 @@
 
 ///v0
 self.addEventListener('install', event => {
-  console.log('Service Worker 安裝完成');
+  console.log('Service Worker install');
   self.skipWaiting(); // 立即進入 activate
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service Worker 啟動');
+  console.log('Service Worker activate');
   event.waitUntil(clients.claim()); // 接管所有頁面
 });
 
 self.addEventListener('fetch', event => {
-  // 最小可用 SW
-}); 
+  console.log('Service Worker fetch');
+  event.respondWith(
+    fetch(event.request) // 直接走網路
+      .catch(() => caches.match(event.request)) // 網路失敗時才用快取
+  );
+});
 
 
 self.addEventListener('push', event => {
@@ -100,6 +104,22 @@ const display = "minimal-ui, browser, standalone, fullscreen";
 ///precacheAndRoute(self.__WB_MANIFEST);
 
 /*  
+
+///v0
+self.addEventListener('install', event => {
+  console.log('Service Worker 安裝完成');
+  self.skipWaiting(); // 立即進入 activate
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker 啟動');
+  event.waitUntil(clients.claim()); // 接管所有頁面
+});
+
+self.addEventListener('fetch', event => {
+  // 最小可用 SW
+}); 
+
 ///V1
 self.addEventListener('install', event => {
   console.log('Service Worker 安裝完成');
