6445edb56a4762e5d279af77ba1a470c7c4e3bb4
duguaynins <duguaynins@gmail.com>
2026-04-12 17:50:20 +0800

    Update nins-world.js

--- children/0001/nins-world.js
+++ children/0001/nins-world.js
@@ -18,6 +18,7 @@ document.body.insertAdjacentHTML('beforeend', `
 
 document.getElementById('merryXmas').innerHTML = "☃️🦌🎄✨⭐<br>[ © nins ] x [ Merry Xmas Snow ]";
 document.getElementById('ninsForms').innerHTML = `[<a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSct8PaArwmkAv10Eh5CEybu_88IoSj9V_QOLy07KiL6efu10g/viewform">Co-founders</a>]`;
+document.getElementById('ninsForms').innerHTML = `[<a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLScFvHqSal-_GkrtrUIG7v3dXmk0aubYma2DCnl-aXBPjWW-ng/viewform">Co-founders</a>]`;
 
 document.addEventListener('click', function (e) {
     if (e.target.tagName === 'BUTTON') {
